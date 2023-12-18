2da1ff7cb28d483c888a81907f1c89dec0a679fe
panda-cat <90021060+panda-cat@users.noreply.github.com>
2023-12-18 17:23:39 +0800

    Update mdev_time.py

--- exec/mdev_time.py
+++ exec/mdev_time.py
@@ -1,101 +1,94 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from concurrent.futures import ThreadPoolExecutor
 import netmiko
-import os
-import datetime
-from threading import Lock
+import threading
 import pandas as pd
 import getopt
-import sys
+import os
+import datetime
 
-class net_dev():
+def load_excel(excel_file):
+    df = pd.read_excel(excel_file,sheet_name="Sheet1")
+    devices_info = df.to_dict(orient="records")
+    return devices_info
 
-    def __init__(self,excel_name):
-        try :
-            os.mkdir("./result"+'{0:%Y%m%d}'.format(datetime.datetime.now()))
-        except:
-            pass
-        self.excel_name = excel_name
-        self.list = [] # 空列表存储设备信息数据
-        self.pool = ThreadPoolExecutor(16) # 初始化线程数量
-        self.lock = Lock()  # 添加线程锁，避免写入数据丢失
-        self.path = ("./result"+'{0:%Y%m%d}'.format(datetime.datetime.now()))  # 创建保存result路径
-        #self.mult_config=[] # 创建列表，保存多条命令。用于批量执行命令
-
-    def get_dev_info(self):
-        # 获取sheet(设备信息)的dataframe.
-        df = pd.read_excel(self.excel_name,sheet_name="Sheet1") # 读取excel的sheet1
-        self.list = df.to_dict(orient="records")  # 将数据打印出来，已字典存储的列表数据
-        #self.mult_config = list(df['mult_command'].split(";"))
-        #mult_conf = df["mult_command"].values.tolist()  # 取一列的值生成列表
-        #print(self.list)
-
-        # 获取sheet(CMD)的dataframe
-        #df1 = pd.read_excel(self.excel_name,sheet_name="Sheet1")
-        #result1 = df1.to_dict(orient="list")  # 将数据打印出来,将一列的数据存为一个字典
-        #self.mult_config = result1["mult_command"]
-        #print(self.mult_config)
-
-    def mult_cmd_in(self,ip,user,dev_type,passwd,secret,cmds,read_time):
-        try:
-            devices = {
-                'device_type': dev_type,  # 锐捷os:ruijie_os, 华三：hp_comware 中兴：zte_zxros
+def execute_commands(ip,user,dev_type,passwd,secret,read_time,cmds):
+    try:
+        net_devices = {
+                'device_type': dev_type,
                 'host': ip,
                 'username': user,
                 'password': passwd,
                 'secret': secret,
                 'read_timeout_override': read_time,
             }
+        net_connect = netmiko.ConnectHandler(**net_devices)
+        if net_devices['secret'] != "huawei" or net_devices['secret'] != "huawei_telnet":
+           connect_dev.enable()
+        if net_devices['secret'] != "hp_comware" or net_devices['secret'] != "hp_comware_telnet":
+           connect_dev.enable()
+        cmd_out = connect_dev.send_multiline(cmds)
+        os.chdir(os.mkdir("./result"+'{0:%Y%m%d}'.format(datetime.datetime.now())))
+        with open (ip + ".txt", "w",encoding="utf-8")  as tmp_fle:
+             tmp_fle.write(cmd_out+'\n')
+        print(ip + " 执行成功")
+        return cmd_out
+    except netmiko.exceptions.NetmikoAuthenticationException:
+        with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
+             failed_ip.write(ip + "  用户名密码错误\n")
+             print(ip + " 用户名密码错误")
+             return None
+    except netmiko.exceptions.NetmikoTimeoutException:
+        with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
+             failed_ip.write(ip + "       登录超时\n")
+             print(ip + " 登录超时")
+             return None
 
-            connect_dev = netmiko.ConnectHandler(**devices)
-            if devices['secret'] != "":
-               connect_dev.enable()
-            #for cmd in cmds:
-            cmd_out = connect_dev.send_multiline(cmds)
-            with open (ip + ".txt", "w",encoding="utf-8")  as tmp_fle:
-                 tmp_fle.write(cmd_out+'\n')
-            print(ip + " 执行成功")
+def multithreaded_execution(devices, num_threads):
+    threads = []
+    for dev_info in devices:
+        cmds = list(dev_info['mult_command'].split(";"))
+        ip = dev_info["host"]
+        user = dev_info["username"]
+        dev_type = dev_info["device_type"]
+        passwd = dev_info["password"]
+        secret = dev_info["secret"]
+        read_time = dev_info["readtime"]
+        t = threading.Thread(target=execute_commands, args=(ip,user,dev_type,passwd,secret,read_time,cmds))
+        threads.append(t)
 
-        except netmiko.exceptions.NetmikoAuthenticationException:
-            self.lock.acquire()
-            with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
-                failed_ip.write(ip + "  用户名密码错误\n")
-                print(ip + " 用户名密码错误")
-            self.lock.release()
-        except netmiko.exceptions.NetmikoTimeoutException:
-            self.lock.acquire()
-            with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
-                failed_ip.write(ip + "       登录超时\n")
-                print(ip + " 登录超时")
-            self.lock.release()
+    # Start threads
+    for t in threads:
+        t.start()
 
-    def main(self):
-        for dev_info in self.list:
-            cmds = list(dev_info['mult_command'].split(";"))
-            #print(dev_info)
-            ip = dev_info["host"]
-            #print(ip)
-            user = dev_info["username"]
-            dev_type = dev_info["device_type"]
-            passwd = dev_info["password"]
-            secret = dev_info["secret"]
-            read_time = dev_info["readtime"]
-            self.pool.submit(self.mult_cmd_in,ip,user,dev_type,passwd,secret,cmds,read_time)
-        os.chdir(self.path)
-        self.pool.shutdown(True)
+    # Wait for threads to complete
+    for t in threads:
+        t.join()
 
-#filename = input("输入设备信息(excel文件):")
-opts,args = getopt.getopt(sys.argv[1:],'-h-i',['help','inputfile'])
-for opt_name,opt_value in opts:
-    if opt_name in ('-h','--help'):
-        print("usage: connexec.exe -i *.xlsx")
-        sys.exit()
-    if opt_name in ('-i','--inputfile'):
-        filename = opt_value
-        print("设备信息excel文件：",filename)
-        my_use = net_dev(filename)
-        my_use.get_dev_info()
-        my_use.main()
+    return
 
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "c:t:", ["excel=", "threads="])
+    except getopt.GetoptError:
+        print("Usage: program_network_devices.py -c <excel_file> -t <num_threads>")
+        sys.exit(2)
+
+    execl_file = ""
+    num_threads = 4
+    for opt, arg in opts:
+        if opt in ("-c", "--excel"):
+            execl_file = arg
+        elif opt in ("-t", "--threads"):
+            num_threads = int(arg)
+
+    devices = load_execl(execl_file)
+    #commands = ["show version", "show ip interface brief"]
+
+    #multithreaded_execution(devices, commands, num_threads)
+    multithreaded_execution(devices, num_threads)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
