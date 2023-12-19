a485a9c8a066c854dd1300f1fee2584f57e83cf9
panda-cat <90021060+panda-cat@users.noreply.github.com>
2023-12-19 12:22:17 +0800

    Update mdev_time.py

--- exec/mdev_time.py
+++ exec/mdev_time.py
@@ -1,75 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import netmiko
-import threading
+import multiprocessing
 import pandas as pd
 import getopt
 import os
 import datetime
+
 
 def load_excel(excel_file):
     df = pd.read_excel(excel_file, sheet_name="Sheet1")
     devices_info = df.to_dict(orient="records")
     return devices_info
 
-def execute_commands(ip, user, dev_type, passwd, secret, read_time, cmds):
+
+def execute_commands(device_info):
+    ip = device_info["host"]
+    user = device_info["username"]
+    dev_type = device_info["device_type"]
+    passwd = device_info["password"]
+    secret = device_info["secret"]
+    read_time = device_info["readtime"]
+    cmds = list(device_info['mult_command'].split(";"))
+
     try:
         net_devices = {
-                'device_type': dev_type,
-                'host': ip,
-                'username': user,
-                'password': passwd,
-                'secret': secret,
-                'read_timeout_override': read_time,
-            }
+            'device_type': dev_type,
+            'host': ip,
+            'username': user,
+            'password': passwd,
+            'secret': secret,
+            'read_timeout_override': read_time,
+        }
         net_connect = netmiko.ConnectHandler(**net_devices)
-        if net_devices['secret'] != "huawei" or net_devices['secret'] != "huawei_telnet":
-           net_connect.enable()
-        if net_devices['secret'] != "hp_comware" or net_devices['secret'] != "hp_comware_telnet":
-           net_connect.enable()
-        cmd_out = net_connect.send_multiline(cmds)
+        if dev_type == "paloalto_panos":
+            cmd_out = net_connect.send_multiline(cmds, expect_string=r">")
+        elif dev_type in ("huawei", "huawei_telnet", "hp_comware", "hp_comware_telnet"):
+            cmd_out = net_connect.send_multiline(cmds)
+        else:
+            net_connect.enable()
+            cmd_out = net_connect.send_multiline(cmds)
+
         os.makedirs(f"./result{datetime.datetime.now():%Y%m%d}", exist_ok=True)
-        with open (os.path.join(f"./result{datetime.datetime.now():%Y%m%d}", f"{ip}.txt"), "w", encoding="utf-8")  as tmp_fle:
-             tmp_fle.write(cmd_out+'\n')
+        with open(os.path.join(f"./result{datetime.datetime.now():%Y%m%d}", f"{ip}.txt"), "w", encoding="utf-8") as tmp_fle:
+            tmp_fle.write(cmd_out + '\n')
         print(f"{ip} 执行成功")
         return cmd_out
+
     except netmiko.exceptions.NetmikoAuthenticationException:
         with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
-             failed_ip.write(f"{ip}  用户名密码错误\n")
-             print(f"{ip} 用户名密码错误")
-             return None
+            failed_ip.write(f"{ip} 用户名密码错误\n")
+        print(f"{ip} 用户名密码错误")
     except netmiko.exceptions.NetmikoTimeoutException:
         with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
-             failed_ip.write(f"{ip}       登录超时\n")
-             print(f"{ip} 登录超时")
-             return None
+            failed_ip.write(f"{ip}    登录超时\n")
+        print(f"{ip} 登录超时")
+
+    return None
+
 
 def multithreaded_execution(devices, num_threads):
-    threads = []
-    for dev_info in devices:
-        cmds = list(dev_info['mult_command'].split(";"))
-        ip = dev_info["host"]
-        user = dev_info["username"]
-        dev_type = dev_info["device_type"]
-        passwd = dev_info["password"]
-        secret = dev_info["secret"]
-        read_time = dev_info["readtime"]
-        t = threading.Thread(target=execute_commands, args=(ip,user,dev_type,passwd,secret,read_time,cmds))
-        threads.append(t)
+    with multiprocessing.Pool(processes=num_threads) as pool:
+        pool.map(execute_commands, devices)
 
-    # Start threads
-    for t in threads:
-        t.start()
-
-    # Wait for threads to complete
-    for t in threads:
-        t.join()
-
-    return
 
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "c:t:", ["excel=", "threads="])
     except getopt.GetoptError:
-        print("Usage: program_network_devices.py -c <excel_file> -t <num_threads>")
+        print("Usage: connexec -c <excel_file> -t <num_threads , default:4>")
         sys.exit(2)
 
     excel_file = ""
@@ -81,9 +81,6 @@ def main(argv):
             num_threads = int(arg)
 
     devices = load_excel(excel_file)
-    #commands = ["show version", "show ip interface brief"]
-
-    #multithreaded_execution(devices, commands, num_threads)
     multithreaded_execution(devices, num_threads)
 
 
