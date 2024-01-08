365cec5cebef3360206021dedd5cd717ef7ce953
panda-cat <90021060+panda-cat@users.noreply.github.com>
2024-01-08 15:45:46 +0800

    Update mdev_time.py

--- exec/mdev_time.py
+++ exec/mdev_time.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import netmiko
+import argparse
+import logging
 import multiprocessing
+import netmiko
 import pandas as pd
-import getopt
 import os
 import datetime
 from concurrent.futures import ThreadPoolExecutor
-
 
 def load_excel(excel_file):
     df = pd.read_excel(excel_file, sheet_name="Sheet1")
     devices_info = df.to_dict(orient="records")
     return devices_info
 
-
-def execute_commands(devices):
-    ip = devices["host"]
-    user = devices["username"]
-    dev_type = devices["device_type"]
-    passwd = devices["password"]
-    secret = devices["secret"]
-    read_time = devices["readtime"]
-    cmds = list(devices["mult_command"].split(";"))
+def execute_commands(mdev):
+    ip = mdev["host"]
+    user = mdev["username"]
+    dev_type = mdev["device_type"]
+    passwd = mdev["password"]
+    devsecret = mdev["secret"]
+    read_time = mdev.get("readtime", 10)
+    cmds = list(mdev["mult_command"].split(";"))
 
     try:
         net_devices = {
@@ -31,60 +30,62 @@ def execute_commands(devices):
             "host": ip,
             "username": user,
             "password": passwd,
-            "secret": secret,
+            "secret": devsecret,
             "read_timeout_override": read_time,
         }
         net_connect = netmiko.ConnectHandler(**net_devices)
-        if dev_type == "paloalto_panos":
-            cmd_out = net_connect.send_multiline(cmds, expect_string=r">", cmd_verify=False)
-        elif dev_type in ("huawei", "huawei_telnet", "hp_comware", "hp_comware_telnet"):
-            cmd_out = net_connect.send_multiline(cmds, cmd_verify=False)
-        else:
-            net_connect.enable()
-            cmd_out = net_connect.send_multiline(cmds, cmd_verify=False)
 
-        output_dir = f"./result{datetime.datetime.now():%Y%m%d}"
+        with net_connect:
+            if dev_type == "paloalto_panos":
+                cmd_out = net_connect.send_multiline(cmds, expect_string=r">", cmd_verify=False)
+            elif dev_type in ("huawei", "huawei_telnet", "hp_comware", "hp_comware_telnet"):
+                cmd_out = net_connect.send_multiline(cmds, cmd_verify=False)
+            elif mdev['secret'] != "":
+                net_connect.enable()
+                cmd_out = net_connect.send_multiline(cmds, cmd_verify=False)
+            else:
+                cmd_out = net_connect.send_multiline(cmds, cmd_verify=False)
+
+        output_dir = args.output
+        if not output_dir:
+            output_dir = f"./result{datetime.datetime.now():%Y%m%d}"
+
         os.makedirs(output_dir, exist_ok=True)
         with open(os.path.join(output_dir, f"{ip}.txt"), "w", encoding="utf-8") as tmp_fle:
             tmp_fle.write(cmd_out + "\n")
-        print(f"{ip} 执行成功")
+        logging.info(f"{ip} 执行成功")
         return cmd_out
 
     except netmiko.exceptions.NetmikoAuthenticationException:
         with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
             failed_ip.write(f"{ip} 用户名密码错误\n")
-        print(f"{ip} 用户名密码错误")
+        logging.error(f"{ip} 用户名密码错误")
     except netmiko.exceptions.NetmikoTimeoutException:
         with open("登录失败列表", "a", encoding="utf-8") as failed_ip:
             failed_ip.write(f"{ip} 登录超时\n")
-        print(f"{ip} 登录超时")
+        logging.error(f"{ip} 登录超时")
 
     return None
-
 
 def multithreaded_execution(devices, num_threads):
     with ThreadPoolExecutor(num_threads) as pool:
         pool.map(execute_commands, devices)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Path to the Excel file containing device information", required=True)
+    parser.add_argument(
+        "-t", "--threads", help="Number of threads to use for parallel execution", type=int, default=4
+    )
+    parser.add_argument(
+        "-o", "--output", help="Directory to save the command outputs. If not specified, a date-based directory will be created in the current directory."
+    )
+    args = parser.parse_args()
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "c:t:", ["excel=", "threads="])
-    except getopt.GetoptError:
-        print("Usage: connexec -c <excel_file> -t <num_threads default:4>")
-        sys.exit(2)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    excel_file = ""
-    num_processes = 4
-    for opt, arg in opts:
-        if opt in ("-c", "--excel"):
-            excel_file = arg
-        elif opt in ("-t", "--threads"):
-            num_threads = int(arg)
-
-    devices = load_excel(excel_file)
-    multithreaded_execution(devices, num_threads)
-
+    devices = load_excel(args.input)
+    multithreaded_execution(devices, args.threads)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
